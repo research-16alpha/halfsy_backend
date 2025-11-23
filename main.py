90ad3fed61bd804edec965cbf2c05b5a890ab18e
research-16alpha <research@16alpha@gmail.com>
2025-11-23 21:12:20 +0530

    updated_domains

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ app = FastAPI(title="Halfsy API")
 # CORS middleware to allow frontend to access the API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.halfsyshop.com"],  # In production, replace with your frontend URL
+    allow_origins=["https://www.halfsyshop.com",
+    "https://www.halfsy.shop",
+    "https://halfsy.shop"],  # In production, replace with your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
